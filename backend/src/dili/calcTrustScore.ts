7c79ac8142016a65d0475d6fc4276d7e55cd3cbd
nhestrompia <nhestrompia@gmail.com>
2023-10-13 22:25:34 +0300

    feat: trust store calculation and wrapper

---
+++ backend/src/dili/calcTrustScore.ts
@@ -0,0 +1,80 @@
+import { NextFunction, Request, Response } from "express";
+import { logger } from "../index.js";
+import Joi from "joi";
+import { supabase } from "../config.js";
+import {
+  dateDiffInDays,
+  getWeb3BioNextId,
+  getAllAttestations,
+} from "./util.js";
+
+const schemaCheck = Joi.object({
+  account: Joi.string().required(),
+  ops: Joi.string().optional(),
+});
+
+const MAX_SCORE_CALC_AGE = 30;
+
+// Collects metadata about the uploaded media and adds it to the manual review inbox
+export const calcTrustScore = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // Attestation is verified in middleware before this function is called
+  logger.debug("Start trust score calc:", req.body);
+
+  const { error: validationError, value } = schemaCheck.validate(req.body);
+  if (validationError) {
+    logger.error("Attestation request validation error:", validationError);
+    return res.status(400).send(validationError.details[0].message);
+  }
+
+  //  console.log("why ");
+  //  console.log("why ");
+  // TODO Add signature verification here to make sure a rando is not spamming us.
+  1 === 1;
+  console.log(req.body && req.body.ops !== "recalc");
+  if (req.body && req.body.ops !== "recalc") {
+    const { data, error } = await supabase
+      .from("user_trust_score")
+      .select("*")
+      .eq("pk", req.body.account)
+      .limit(1)
+      .single();
+    if (data !== null && data.updated_at !== null) {
+      let last = new Date(Date.parse(data.updated_at));
+      let today = new Date();
+      console.log(dateDiffInDays(last, today));
+      if (dateDiffInDays(last, today) < MAX_SCORE_CALC_AGE) {
+        return res.status(200).send(data);
+      }
+    }
+  }
+
+  // Check in DB if they have  gitcoin passport
+  // Check in DB if not there then check in EAS graphQL
+  // check if they have talent layer reviews (which right now nobody does)
+  // check if they have our attestations
+
+  // get domains from next.id
+
+  //const res1= await getWeb3BioNextId(req.body.account);
+  const res2 = await getAllAttestations(req.body.account);
+  /*
+  const { data } = await supabase
+  .from("attestations")
+  .select("*");
+  */
+  1 === 1;
+
+  //AirStack is so hard to get a clean answer out of
+
+  const ll = await supabase.from("attester_a_priori_trust_coef").select("*");
+
+  ll.data;
+
+  console.log("Approved request for ", ll.data);
+
+  return res.status(200).send({ data: "HELLO WORLD" });
+};
--- backend/src/dili/util.ts
+++ backend/src/dili/util.ts
@@ -5,7 +5,6 @@ import { supabase } from "../config.js";
 
 import axios from "axios";
 
-
 /*
 
 // Collects metadata about the uploaded media and adds it to the manual review inbox
@@ -20,20 +19,20 @@ export const calcTrustScore = async (
   };  
 
 
-  */ 
+  */
 
-  const MAX_SCORE_CALC_AGE=30 ; //TODO move to config 
+const MAX_SCORE_CALC_AGE = 30; //TODO move to config
 
-  export function dateDiffInDays(a:Date, b:Date) {  
-    const _MS_PER_DAY = 1000 * 60 * 60 * 24;
-    // Discard the time and time-zone information.
-    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
-    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
-  
-    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
-  }
+export function dateDiffInDays(a: Date, b: Date) {
+  const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+  // Discard the time and time-zone information.
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
 
-  /*
+  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
+
+/*
 export const rest_get_and_save_to_db= async(url:string, headers:object={Accept: 'application/json', "content-type":'application/json'}, callback:any =(response: any) => {}, max_age_days:number=30, ops?:string)=>{
 
 
@@ -75,62 +74,121 @@ export const rest_get_and_save_to_db= async(url:string, headers:object={Accept: 
 }
 */
 
-export const rest_api_save_to_db= async(url:string, method:string, req_body:object={}, headers:object={Accept: 'application/json', "content-type":'application/json'}, callback:any =(response: any) => {}, max_age_days:number=30, ops?:string)=>{
+export const rest_api_save_to_db = async (
+  url: string,
+  method: string,
+  req_body: object = {},
+  headers: object = {
+    Accept: "application/json",
+    "content-type": "application/json",
+  },
+  callback: any = (response: any) => {},
+  max_age_days: number = 30,
+  ops?: string
+) => {
+  if (ops != "recalc") {
+    const { data } = await supabase
+      .from("rest_cache")
+      .select("*")
+      .eq("url", url)
+      .eq("method", method)
+      .eq("req_body", req_body)
+      .single();
 
-
-
-    if(ops!="recalc"){
-        const {data} = await supabase
-        .from("rest_cache")
-        .select("*")
-        .eq("url",url)
-        .eq("method",method)
-        .eq("req_body",req_body)
-        .single();
- 
-            if(data!==null&&data.updated_at!==null){
-                let last=  new Date(Date.parse(data.updated_at));
-                let today= new Date();
-                console.log( dateDiffInDays(last,today))
-                if( dateDiffInDays(last,today) <max_age_days  ){
-                    callback(data.req_body);
-                    return data.req_body;
-                } 
-    
-        
-        }
+    if (data !== null && data.updated_at !== null) {
+      let last = new Date(Date.parse(data.updated_at));
+      let today = new Date();
+      console.log(dateDiffInDays(last, today));
+      if (dateDiffInDays(last, today) < max_age_days) {
+        callback(data.req_body);
+        return data.req_body;
+      }
     }
+  }
 
-    let config = {
-        method: method,
-        maxBodyLength: Infinity,
-        url: url,
-        headers: headers,
-        body:req_body
-      };
+  let config = {
+    method: method,
+    maxBodyLength: Infinity,
+    url: url,
+    headers: headers,
+    data: req_body,
+  };
 
-      // @ts-ignore
+  1 === 1;
+  // @ts-ignore
+  /*
      async function save_res_to_db(url,res){
         const {data} = await supabase
         .from("get_rest_cache")
         .upsert({url:url,method:method,req_body:req_body, response:res.response_body,status:res.status})
      }
-      await axios.request(config).then((response) => {save_res_to_db(url,response); callback(response); return(response)}).catch((error) => {console.error(error);});
-}
+     */
+
+  1 === 1;
+  //await axios.request(config).then((response) => {save_res_to_db(url,response); callback(response); return(response)}).catch((error) => {console.error(error);});
+
+  let res = await axios.request(config);
+  1 == 1;
+  if (req_body === undefined || req_body === null) req_body = {};
+  // @ts-ignore
+  const { data, error } = await supabase
+    .from("rest_cache")
+    .upsert({
+      url: url,
+      method: method,
+      req_body: req_body,
+      response_body: res.data.data,
+      status: res.status,
+    });
+
+  console.log(error);
+  1 === 1;
+  return res;
+};
+
+export const getWeb3BioNextId = async (pk: string) => {
+  await rest_api_save_to_db("https://api.web3.bio/profile/" + pk, "get");
+};
+
+//For now this is only looking on optimism ,  but we should do all other chains
+export const getAllAttestations = async (pk: string) => {
+  //let getall_attestions_filter_receipiant_pk=JSON.parse('{"query":"query Query($where: AttestationWhereInput) {  attestations(where: $where) {    id    data    attester    decodedDataJson    expirationTime    ipfsHash    isOffchain    recipient    refUID    revocable    revocationTime    revoked    schemaId    time    timeCreated    txid  }}","variables":{"where":{"recipient":{"equals":"'+pk+'"}}}}');
+
+  let query =
+    "query Query($where: AttestationWhereInput) {  attestations(where: $where) {    id    data    attester    decodedDataJson    expirationTime    ipfsHash    isOffchain    recipient    refUID    revocable    revocationTime    revoked    schemaId    time    timeCreated    txid  }}";
+  let variables = { where: { recipient: { equals: pk } } };
+
+  console.log("LOOOK HERE variables=" + JSON.stringify(variables));
+
+  //let res =await axios.post("https://optimism.easscan.org/graphql",{query:query,variables:variables})
+
+  await rest_api_save_to_db(
+    "https://optimism.easscan.org/graphql",
+    "post",
+    { query: query, variables: variables },
+    {}
+  );
+  1 === 1;
+};
+
+/*
+curl --request POST \
+  --header 'content-type: application/json' \
+  --url 'https://optimism.easscan.org/graphql' \
+  --data '{"query":"query { __typename }"}'
+
+*/
+
+/*
+curl --request POST \
+    --header 'content-type: application/json' \
+    --url 'https://optimism.easscan.org/graphql' \
+    --data '{"query":"query Query($where: AttestationWhereInput) {  attestations(where: $where) {    id    data    attester    decodedDataJson    expirationTime    ipfsHash    isOffchain    recipient    refUID    revocable    revocationTime    revoked    schemaId    time    timeCreated    txid  }}","variables":{"where":{"recipient":{"equals":"0xA00c50A0A97D7b4d03e7Ff4A8C1badf61d72816f"}}}}'
 
 
-export const getWeb3BioNextId = async ( pk : string)=>{
 
-    await rest_api_save_to_db("https://api.web3.bio/profile/"+pk,"get")
-
-}
-
-
-//For now this is only looking on optimism ,  but we should do all other chains 
-export const getAllAttestations = async ( pk : string)=>{
-
-    let getall_attestions_filter_receipiant_pk=JSON.parse(`'{"query":"query Query($where: AttestationWhereInput) {\n  attestations(where: $where) {\n    id\n    data\n    attester\n    decodedDataJson\n    expirationTime\n    ipfsHash\n    isOffchain\n    recipient\n    refUID\n    revocable\n    revocationTime\n    revoked\n    schemaId\n    time\n    timeCreated\n    txid\n  }\n}","variables":{"where":{"recipient":{"equals":"${pk}"}}}}'`);
-
-    await rest_api_save_to_db("https://optimism.easscan.org/graphql","post",getall_attestions_filter_receipiant_pk)
-
-}
+curl --request POST \
+    --header 'content-type: application/json' \
+    --url 'https://optimism.easscan.org/graphql' \
+    --data '{"query":"query Query($where: AttestationWhereInput) {  attestations(where: $where) {    id    data    attester    decodedDataJson    expirationTime    ipfsHash    isOffchain    recipient    refUID    revocable    revocationTime    revoked    schemaId    time    timeCreated    txid  }}","variables":{"where":{"recipient":{"equals":"0xA00c50A0A97D7b4d03e7Ff4A8C1badf61d72816f"}}}}'
+    */
