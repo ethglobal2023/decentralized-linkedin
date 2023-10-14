155906541a27eabf34007e8037e82de6e70b9231
A A <20057155+ad0ll@users.noreply.github.com>
2023-10-14 17:21:26 -0400

    Add batch verify

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -2,25 +2,35 @@ import {
   AttestationShareablePackageObject,
   Offchain,
   OFFCHAIN_ATTESTATION_VERSION,
-  PartialTypedDataConfig, SchemaEncoder, SchemaItem
+  PartialTypedDataConfig,
 } from "@ethereum-attestation-service/eas-sdk";
 import { hexlify, Signature, verifyTypedData } from "ethers";
 
 export const validAttestation = (
-  request: AttestationShareablePackageObject
+  document: AttestationShareablePackageObject
 ) => {
   const EAS_CONFIG: PartialTypedDataConfig = {
-    address: request.sig.domain.verifyingContract,
-    version: request.sig.domain.version,
-    chainId: request.sig.domain.chainId,
+    address: document.sig.domain.verifyingContract,
+    version: document.sig.domain.version,
+    chainId: document.sig.domain.chainId,
   };
 
   // const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Sepolia v0.26
   const offchain = new Offchain(EAS_CONFIG, OFFCHAIN_ATTESTATION_VERSION);
   return offchain.verifyOffchainAttestationSignature(
-    request.signer,
-    request.sig
+    document.signer,
+    document.sig
   );
+};
+
+export const batchVerifyAttestations = (
+  documents: AttestationShareablePackageObject[]
+) => {
+  for (const attestation of documents) {
+    if (!validAttestation(attestation)) {
+      return false;
+    }
+  }
 };
 
 export const extractAddressFromAttestation = (
